896d36d03a9b2ea8429be6b2cd152d05acf31df4
André Pelisoli <andre.pelisoli@somoseducacao.com.br>
2020-09-05 11:07:58 -0300

    Adding cards to represent Transaction List and Graph sections

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,24 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(Object context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("teste"),
-      ),
-      body: Center(
-        child: Text('teste'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Expenses"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Graph'),
+              ),
+            ),
+            Card(
+              child: Text('Transaction List'),
+            )
+          ],
+        ));
   }
 }
